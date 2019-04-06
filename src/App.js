ee79c9f074b101a1b3942acdd651a35e98593bc0
pawanman56 <pawanman56@gmail.com>
2019-04-06 20:45:53 +0545

    basic useState

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 export default function App() {
+  // const [state, function] = useState();
+  const [cartItems, setCartItems] = useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React Hooks!
+          Items in your cart: {cartItems.toString()}
         </p>
+        <button
+          onClick={() => {
+            // use spread operator to update the value
+            setCartItems([...cartItems, 'apple'])
+          }} 
+        >
+          Add to cart
+        </button>
       </header>
     </div>
   );
